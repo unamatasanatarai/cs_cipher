61d199173e8515810d69c3a5b3adfbea27690912
Unamata <unamatasanatarai@gmail.com>
2016-11-05 12:33:17 +0100

    Hello World

---
+++ Cipher/Credits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cipher
+{
+    class Credits
+    {
+        static public void Display()
+        {
+            Console.Clear();
+            Console.WriteLine("+-------------------------------------------+");
+            Console.WriteLine("|                                           |");
+            Console.WriteLine("|  Szyfrowanie                              |");
+            Console.WriteLine("|                                           |");
+            Console.WriteLine("|  .autor                                   |");
+            Console.WriteLine("|  Piotr Grabowski                          |");
+            Console.WriteLine("|                               05.11.2016  |");
+            Console.WriteLine("|                                           |");
+            Console.WriteLine("+-------------------------------------------+");
+            Console.WriteLine();
+
+        }
+    }
+}
